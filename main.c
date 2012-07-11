26ca1f4f136335720f4a3c79fb05ac0c25e00280
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-07-11 10:56:05 +0200

    Read the mac address from macaddr.txt

--- main.c
+++ main.c
@@ -49,23 +49,9 @@
 #include "gpio.h"
 #include "include/AT91SAM9GX5_inc.h"
 
-// The Magic number can be used to find and to change on-the-fly the board 
-// MAC address 
-
-static struct {
-	unsigned int magic_number;
-	unsigned char mac_address[6];
-} magic_and_mac = {
-	0x5C5C5C5C,
-	{0x00, 0x04, 0x25, 0x12, 0x34, 0x56}
-};
-
-
-
-void user_hw_init(void);
-extern void Jump(unsigned int addr);
-extern unsigned int load_SDCard(void *dst);
 void LoadLinux();
+void LoadApp();
+extern unsigned int load_SDCard(unsigned char *,char *);
 
 /*------------------------------------------------------------------------------*/
 /* Function Name       : Wait							*/
@@ -79,55 +65,90 @@ void Wait(unsigned int count)
         asm volatile ("    nop");
 }
 
+// Convert a hex char in a binary value
+// ex: '0' ret 0x30
+// ex: 'A' ret 0x41
+unsigned char c2b(unsigned char value) {
+	if (value>='0' && value<='9') return value-0x30; 
+	if (value>='A' && value<='F') return value-0x41+0x0A; 
+	return 0;
+}
+
 int main(void)
 {
+	// Dummy buffer to load the MAC address from file
+	unsigned char mac_buffer[100];
+	unsigned char mac_address[6];
+
+	// Register used by the CPU to save the MAC address
 	unsigned int SA1L_value;
 	unsigned int SA1H_value;
 
 	// Board hardware initialization
     hw_init();
+
+	// Turn-on the green led on Aria G25 board
+	pio_set_gpio_output(AT91C_PIN_PB(8),1);
+
+	// Set here the default GPIO config you need at reset
+	pio_set_gpio_input(AT91C_PIN_PA(7),1);
+	pio_set_gpio_input(AT91C_PIN_PA(11),1);
+	pio_set_gpio_input(AT91C_PIN_PA(12),1);
+	pio_set_gpio_input(AT91C_PIN_PA(13),1);
+	pio_set_gpio_input(AT91C_PIN_PA(14),1);
+
+	// First messages on the debug port
     dbgu_print("\n\r");
-    dbgu_print("AriaBoot Version 0.08\n\r");
+    dbgu_print("AriaBoot Version 0.18\n\r");
 
-    /*
-     * Se the Ethernet MAC address
-     */
+	// Load the ethernet MAC address from macaddress.txt
+    if (load_SDCard(mac_buffer,"macaddr.txt")==1) {
+		mac_buffer[17]=0;
+		dbg_log(0,"Ethernet MAC address: %s\n\r",mac_buffer);
 
-	dbg_log(3,"Ethernet MAC address: %02X%02X%02X%02X%02X%02X\n\r",
-		magic_and_mac.mac_address[0],  magic_and_mac.mac_address[1],  magic_and_mac.mac_address[2],
-		magic_and_mac.mac_address[3],  magic_and_mac.mac_address[4],  magic_and_mac.mac_address[5]);
+		mac_address[0]=(c2b(mac_buffer[0])<<4)+c2b(mac_buffer[1]);
+		mac_address[1]=(c2b(mac_buffer[3])<<4)+c2b(mac_buffer[4]);
+		mac_address[2]=(c2b(mac_buffer[6])<<4)+c2b(mac_buffer[7]);
+		mac_address[3]=(c2b(mac_buffer[9])<<4)+c2b(mac_buffer[10]);
+		mac_address[4]=(c2b(mac_buffer[12])<<4)+c2b(mac_buffer[13]);
+		mac_address[5]=(c2b(mac_buffer[15])<<4)+c2b(mac_buffer[16]);
+	} else {
+	    dbgu_print("Set a dummy MAC address\n\r");
+		mac_address[0]=0x00;
+		mac_address[1]=0x04;
+		mac_address[2]=0x25;
+		mac_address[3]=0x12;
+		mac_address[4]=0x34;
+		mac_address[5]=0x56;
+	}
+	
+    // Set the Ethernet MAC address
+	dbg_log(0,"Ethernet MAC address: %x:%x:%x:%x:%x:%x\n\r",
+		mac_address[0],  mac_address[1],  mac_address[2],
+		mac_address[3],  mac_address[4],  mac_address[5]);
 
 	// Enable the MAC0 interface clock	
 	writel(1<<AT91C_ID_EMAC0, AT91C_PMC_PCER);
 
-	SA1L_value=	(unsigned int)magic_and_mac.mac_address[3] << 24 | 
-				(unsigned int)magic_and_mac.mac_address[2] << 16 | 
-				(unsigned int)magic_and_mac.mac_address[1] << 8  | 
-				(unsigned int)magic_and_mac.mac_address[0];
+	SA1L_value=	(unsigned int)mac_address[3] << 24 | 
+				(unsigned int)mac_address[2] << 16 | 
+				(unsigned int)mac_address[1] << 8  | 
+				(unsigned int)mac_address[0];
 
-	SA1H_value=	(unsigned int)magic_and_mac.mac_address[5] << 8  | 
-				(unsigned int)magic_and_mac.mac_address[4];
+	SA1H_value=	(unsigned int)mac_address[5] << 8  | 
+				(unsigned int)mac_address[4];
 
 	// Save the MAC address
 	writel(SA1L_value,AT91C_EMACB0_SA1L);
 	writel(SA1H_value,AT91C_EMACB0_SA1H);
 
-    //dbg_log(3,"AT91C_EMACB0_SA1L = %x \n\r",readl(AT91C_EMACB0_SA1L));
-    //dbg_log(3,"AT91C_EMACB0_SA1H = %x \n\r",readl(AT91C_EMACB0_SA1H));
-
-	// Turn-on the green led on Aria G25 board
-	pio_set_gpio_output(AT91C_PIN_PB(8),1);
-
-	pio_set_gpio_output(AT91C_PIN_PA(7),0);
-	pio_set_gpio_output(AT91C_PIN_PA(11),1);
-	pio_set_gpio_output(AT91C_PIN_PA(12),0);
-	pio_set_gpio_output(AT91C_PIN_PA(13),1);
-	pio_set_gpio_output(AT91C_PIN_PA(14),0);
-
+	// Search for Linux uImage...
     dbgu_print("Read and run the Linux (uImage) from microSD card ...\n\r");
     LoadLinux("uImage");
     dbgu_print("Linux not found !\n\r\n\r");
     dbgu_print("Read and run an user application (userapp.bin) from microSD card ...\n\r");
+
+	// ..or for an user application
     LoadApp("userapp.bin");
     dbgu_print("User application not found !\n\r\n\r");
     dbgu_print("Loop forever...\n\r\n\r");
